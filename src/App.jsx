cddaf7f9f92b5264141fec5f25c0c98840e2f200
이성 <hughlee@idstrust.com>
2025-03-04 07:42:35 +0900

    props로 데이터전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@ import Main from "./commponents/Main.jsx";
 import Button from "./commponents/Button.jsx";
 
 function App() {
-	// 1. props로 전달할 데이터를 정의, 스프레드연산자
 	const buttonProps = {
 		text: "메일",
 		color: "red",
-		a: 1,
-		b: 2,
-		c: 3,
-	};
+		a : 1 ,
+		b : 2 ,
+		c : 3 , 
+};
 
 	return (
 		<>
-			<Button {...buttonProps} />
+			<Button {...buttonProps}/>
 			<Button text={"카페"} />
-			<Button text={"블로그"} />
+			<Button text={"블로그"} > 
+				<Header/>
+			</Button>
 		</>
 	);
 }
--- src/commponents/Button.jsx
+++ src/commponents/Button.jsx
@@ -1,20 +1,15 @@
-const Button = ({ children, text, color = "black" }) => {
-	console.log({ text, color });
-	return (
-		<button style={{ color: color }}>
-			{text} - {color.toUpperCase()}
-		</button>
-	);
+const Button = ({children, text,color="black"}) => {
+	// console.log(props);
+	return <button style={{ color: color }}>
+		{text}-{color.toUpperCase()}{children}
+		</button>;
 };
-// undifined의 값에 점표기표를 사용하면 오류가 발생한다.
 
-// react 18버전부터는 defaultProps를 사용하지 않는다.
-// Button.defaultProps = {
-// 	color : "black",
+// 리액트 18이하인경우
+// Button.defaulPorps = {
+// 	color:"black",
 // }
 
-// props의 기본값을 설정하려면
-// props를 구조분해하고 기본값을 설정하면 된다.
-// const Button = ({ text, color = "black" }) => {
+// 구조할당분석방법을 이용
 
 export default Button;
