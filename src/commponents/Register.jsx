768b7e880374d0064fd2bfccbe299db627834603
이성 <hughlee@idstrust.com>
2025-03-07 07:43:44 +0900

    state로 사용자입력 관리하기, 여러 이벤트핸들러 통합 및 스테이트 통합

--- src/commponents/Register.jsx
+++ src/commponents/Register.jsx
@@ -5,47 +5,104 @@ import { useState } from "react";
 // 3. 국적
 // 4. 자기소개
 const Register = () => {
-  const [name, setName] = useState("이름");
-  const [birth, setBrithe] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  // const [name, setName] = useState("이름");
+  // const [birth, setBrithe] = useState("");
+  // const [country, setCountry] = useState("");
+  // const [bio, setBio] = useState("");
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  // 위에 4개를 1개의 state로 관리하는것으로 변경
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+  console.log(input);
+
+  // 통합이벤트 핸들러
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
-  const onChangeBirth = (e) => {
-    setBrithe(e.target.value);
-  };
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  };
+
+  // const onChangeName = (e) => {
+  //   setInput({
+  //     ...input,
+  //     name: e.target.value,
+  //   });
+  // };
+  // const onChangeBirth = (e) => {
+  //   setInput({
+  //     ...input,
+  //     birth: e.target.value,
+  //   });
+  // };
+  // const onChangeCountry = (e) => {
+  //   setInput({
+  //     ...input,
+  //     country: e.target.value,
+  //   });
+  // };
+  // const onChangeBio = (e) => {
+  //   setInput({
+  //     ...input, //스프레드연산자
+  //     bio: e.target.value,
+  //   });
+  // };
+
+  // const onChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
+  // const onChangeBirth = (e) => {
+  //   setBrithe(e.target.value);
+  // };
+  // const onChangeCountry = (e) => {
+  //   setCountry(e.target.value);
+  // };
+  // const onChangeBio = (e) => {
+  //   setBio(e.target.value);
+  // };
+
+  // setBio, setCount 이런것을 상태변화 함수라고 함
+  // const on ChangeBio 이런것을 이벤트 핸들러라고
 
   return (
     <>
       <div>
         <div>
-          <input value={name} onChange={onChangeName} placeholder={"이름"} />
+          <input
+            name="name"
+            value={input.name}
+            onChange={onChange}
+            placeholder={"이름"}
+          />
         </div>
 
         <div>
-          <input value={birth} type="date" onChange={onChangeBirth} />
+          <input
+            name="birth"
+            value={input.birth}
+            type="date"
+            onChange={onChange}
+          />
         </div>
 
         <div>
-          <select value={country} onChange={onChangeCountry}>
+          <select name="country" value={input.country} onChange={onChange}>
             <option value=""></option>
             <option value="kr">한국</option>
             <option value="us">미국</option>
             <option value="uk">영국</option>
           </select>
-          {country}
+          {input.country}
         </div>
+
         <div>
-          <textarea value={bio} onChange={onChangeBio} />
-          {bio}
+          <textarea name="bio" value={input.bio} onChange={onChange} />
+          {input.bio}
         </div>
       </div>
     </>
